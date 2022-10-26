8ce5c0627d6cd85eced9668c7ea4762e1333cc42
bala.sridhar <bala.sridhar@nwea.org>
2022-10-26 16:40:17 -0500

    added progress bar.

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Progress from './components/Progress';
+
+const marketingLazy = lazy(() => import('./components/MarketingApp'));
+const authLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'con'
@@ -15,10 +17,12 @@ export default () => {
         <StylesProvider generateClassName={generateClassName}>
             <div>
                 <Header />
-                <Switch>
-                    <Route path="/auth" component={AuthApp} />
-                    <Route path="/" component={MarketingApp} />
-                </Switch>
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth" component={authLazy} />
+                        <Route path="/" component={marketingLazy} />
+                    </Switch>
+                </Suspense>
             </div>
         </StylesProvider>
     </BrowserRouter>
