ec80dd84cccba3e5b53d164d301830028faca053
bala.sridhar <bala.sridhar@nwea.org>
2022-10-26 14:26:22 -0500

    updated navigation.

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,22 +1,29 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate }) => {
-    const history = createMemoryHistory();
-    history.listen(onNavigate());
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) { 
+        history.listen(onNavigate);
+    }
     ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 }
 
 if (process.env.NODE_ENV === "development") {
     const devMarRoot = document.querySelector('#_marketing-dev-root');
     if (devMarRoot) {
-        mount(devMarRoot, {
-                onNavigate: () => {
-                console.log("navigation done !!!");
-            }
-        });
+        mount(devMarRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
